f275ed8a07ebe1dfa569e935e5ff4709e74a6e57
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-10-18 15:52:38 +0200

    Create snmpget.py

---
+++ snmpget.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import subprocess
+import os
+import yaml
+import json
+
+with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + 'data.yml', 'r') as yml_file:
+    config = yaml.load(yml_file)
+
+for device_config in config['devices']:
+    host = device_config['host']
+    community = device_config['community']
+    result = []
+    for metric_config in device_config['metrics']:
+        oid = metric_config['oid']
+        name = metric_config['name']
+        p = subprocess.Popen(['snmpget','-v','2c','-c',community,'-Ln','-OQv',host,oid],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        obj = {'host': (host), (name):(out.rstrip('\n'))}
+        print(json.dumps(obj, separators=(', ',': ')))
